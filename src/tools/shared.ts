5cd3ad2bbfb4c1adece743ff71aba82e478bc320
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2023-10-26 16:46:42 +0800

    chore(tools): assign misc data client to window

--- src/tools/shared.ts
+++ src/tools/shared.ts
@@ -81,3 +81,4 @@ export function apkName(version: string) {
 export const CACHE_EXPIRE_TIME = 24 * 60 * 60 * 1000;
 
 export const miscDataClient = new CachedHttpGetClient("misc-data");
+Object.assign(window, { miscDataClient });

f4b0b204212764929d6800383c3e753a840ee6ef
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2024-09-03 01:57:25 +0800

    feat(string): support abbreviation search

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -5,6 +5,9 @@ export const searchMatch = (searchText: string, candidate: string): number | nul
   if (searchText === candidate) {
     return 0;
   }
+  if (searchText.toUpperCase() === abbr(candidate).toUpperCase()) {
+    return 0.5;
+  }
   const index = candidate.toLowerCase().indexOf(searchText.toLowerCase());
   if (index === -1) {
     return null;
@@ -22,3 +25,10 @@ export const trimSlash = (str: string): string => {
 };
 
 export const pad2 = (n: number) => `${n}`.padStart(2, "0");
+
+export const abbr = (s: string) =>
+  s
+    .split(/\s|[\.\-\[\]]/)
+    .map((part) => part.trim().at(0))
+    .filter((c) => !!c)
+    .join("");
